01cc1a6d9c0c24410145eba345a25508c7f76d32
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-28 17:06:30 -0700

    learned to check the time

---
+++ pyqt practice/Prac1.py
@@ -0,0 +1,9 @@
+#from PyQt5.QtCore import QT_VERSION_STR
+#from PyQt5.Qt import PYQT_VERSION_STR
+
+#print(QT_VERSION_STR)
+#print(PYQT_VERSION_STR)
+
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+
+now = QDate.currentDate()
