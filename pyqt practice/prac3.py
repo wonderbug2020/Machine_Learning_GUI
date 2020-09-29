6ae607cd7bf58f5137e74db788df792bb9611f00
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-29 12:13:21 -0700

    created a window that displays data from a csv

--- pyqt practice/prac3.py
+++ pyqt practice/prac3.py
@@ -1,50 +1,49 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
+import pandas as pd
+
+planet_data = pd.read_csv('exoplanet_data.csv')
 
 class TableModel(QtCore.QAbstractTableModel):
+
     def __init__(self, data):
         super(TableModel, self).__init__()
         self._data = data
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-        #if role == Qt.BackgroundRole and index.column() == 2:
-            #return QtGui.QColor('blue')
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
 
     def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
+        return self._data.shape[0]
 
     def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
+        return self._data.shape[1]
 
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
 
 class MainWindow(QtWidgets.QMainWindow):
+
     def __init__(self):
         super().__init__()
 
         self.table = QtWidgets.QTableView()
 
-        data = [
-          [4, 9, 2],
-          [1, 0, 0],
-          [3, 5, 0],
-          [3, 3, 2],
-          [7, 8, 9],
-        ]
+        data = planet_data
 
         self.model = TableModel(data)
         self.table.setModel(self.model)
 
         self.setCentralWidget(self.table)
-
 
 app=QtWidgets.QApplication(sys.argv)
 window=MainWindow()
