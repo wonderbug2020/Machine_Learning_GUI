5d9f71d415fe9adda592628b3a3d6bdb7c479e84
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-09-28 17:40:16 -0700

    made a simple window

--- pyqt practice/Prac1.py
+++ pyqt practice/Prac1.py
@@ -4,6 +4,36 @@
 #print(QT_VERSION_STR)
 #print(PYQT_VERSION_STR)
 
-from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+#from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
 
-now = QDate.currentDate()
+#now = QDate.currentDate()
+
+#print(now.toString(Qt.ISODate))
+#print(now.toString(Qt.DefaultLocaleLongDate))
+
+#datetime = QDateTime.currentDateTime()
+
+#print(datetime.toString())
+
+#time = QTime.currentTime()
+
+#print(time.toString(Qt.DefaultLocaleLongDate))
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
+
+if __name__=='__main__':
+    main()
+"""
