71b0d09ef352f89d5744896d91080366b9d833cf
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-01 17:01:05 -0700

    added template for dataframe editor

--- pyqt practice/v0.1.py
+++ pyqt practice/v0.1.py
@@ -1,6 +1,27 @@
 import sys
-import pandas
-from PyQt5.QtWidgets import QMainWindow, QApplication
+import pandas as pd
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QTableView
+from PyQt5.QtCore import QAbstractTableModel, Qt
+
+pulsar_data = pd.read_csv('data/HTRU_2.csv')
+
+class DataInteractionForm(QWidget, QAbstractTableModel):
+    def __init__(self,data):
+        #super().__init__()
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self):
+        return self._data.shape[0]
+
+    def columnCount(self):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
 
 
 
@@ -10,8 +31,12 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Gotta Start Somewhere')
         self.resize(1200,600)
 
+        data = pulsar_data
+        #print(data.iloc[index.row(),index.column()])
+
         self.menuBar = self.menuBar()
         self.fileMenu = self.menuBar.addMenu('File')
+        self.modelMenu = self.menuBar.addMenu('Model')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
