504c8a6ddaf3ea86808d3578957ca8fba7cd4e27
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-05 18:08:16 -0700

    added a text box descriptor for dropdown. also added comments

--- pyqt practice/v0.1.2.py
+++ pyqt practice/v0.1.2.py
@@ -35,26 +35,34 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        #Sets the title for the main window
         self.setWindowTitle("prototype 2")
 
+        #Loads the data into a df to be used elsewhere
         data = pd.read_csv('data/HTRU_2.csv')
+        #makes a list of all the column headers
         col_headers = data.columns.tolist()
-        print(type(col_headers))
 
+        #Sets up the layouts that will be used
         self.central_widget = QWidget()
         self.layout1 = QHBoxLayout(self.central_widget)
         self.layout2 = QVBoxLayout()
 
+        #Left side table
         self.model = TableModel(data)
         self.table = QtWidgets.QTableView()
         self.table.setModel(self.model)
-        self.layout1.addWidget(self.table,75)
+        self.layout1.addWidget(self.table,80)
 
+        #Right side UI
+        self.dropdown_1_text = QLabel("Select the predictor column")
+        #self.layout2.addWidget(self.dropdown_1_text)
         self.dropdown_1 = QComboBox()
         self.dropdown_1.addItems(col_headers)
+        self.layout2.addWidget(self.dropdown_1_text)
         self.layout2.addWidget(self.dropdown_1)
 
-        self.layout1.addLayout(self.layout2, 25)
+        self.layout1.addLayout(self.layout2, 20)
 
         self.setCentralWidget(self.central_widget)
         #self.setCentralWidget(layout1)
