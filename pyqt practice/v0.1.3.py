94dd37d37df037fbb274b0c96e4f92307616ec93
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-10-16 10:58:47 -0700

    Passed a value from a dropdown box to a function in another class

--- pyqt practice/v0.1.3.py
+++ pyqt practice/v0.1.3.py
@@ -32,31 +32,83 @@ class TableModel(QtCore.QAbstractTableModel):
             if orientation == Qt.Vertical:
                 return str(self._data.index[section])
 
-class UiLayout(QWidget):
-    def __init__(self):
-        super().__init__()
 
-        #self.central_widget = QWidget()
-        #self.layout1 = QHBoxLayout(self.central_widget)
-        #self.dropdown_1_text = QLabel("Select the predictor column")
-        #self.layout1.addWidget(self.dropdown_1_text)
 
-        self.layout_top = QVBoxLayout()
-        self.text_1 = QLabel("text goes here")
-        self.layout_top.addWidget(self.text_1)
+    def get_predictor(self, y):
+        print(f"the selected predictor is {y}")
 
 class MainWindow(QMainWindow):
-    def __init__(self, ui_layout):
+    def __init__(self):
         super(MainWindow, self).__init__()
 
         #Sets the title for the main window
-        self.setWindowTitle("prototype 3")
+        self.setWindowTitle("prototype 2")
 
-        self.setCentralWidget(ui_layout)
+        #Loads the data into a df to be used elsewhere
+        data = pd.read_csv('data/HTRU_2.csv')
+        #makes a list of all the column headers
+        col_headers = data.columns.tolist()
+        #makes a list of the ML models to used
+        model_lst = ['Logistic regression']
+
+        #Sets up the layouts that will be used
+        self.central_widget = QWidget()
+        self.layout1 = QHBoxLayout(self.central_widget)
+        self.layout2 = QVBoxLayout()
+
+        #Left side table
+        self.model = TableModel(data)
+        self.table = QtWidgets.QTableView()
+        self.table.setModel(self.model)
+        self.layout1.addWidget(self.table,80)
+
+        #Right side UI
+        #Dropdown list to choose the column
+        self.dropdown_1_text = QLabel("Select the predictor column")
+        self.dropdown_1 = QComboBox()
+        self.dropdown_1.addItems(col_headers)
+
+        #Dropdown list to choose the model
+        self.dropdown_2_text = QLabel("Select the Model you would like to use")
+        self.dropdown_2 = QComboBox()
+        self.dropdown_2.addItems(model_lst)
+
+        #Input box to set train/test split
+        self.train_split_text = QLabel("Select the train test split you would like to use")
+        self.train_split_input = QLineEdit()
+        self.train_split_input.setMaxLength(3)
+
+        #Button to build the model
+        self.model_btn_text = QLabel("Push here once you have choosen all your options")
+        self.model_btn = QPushButton()
+        self.model_btn.setText("Run Model")
+        self.model_btn.clicked.connect(lambda: self.model.get_predictor(self.dropdown_1.currentText()))
+        
+        #Label to display the results
+        self.output_text = QLabel()
+
+        #adding the widgets to the layout
+        self.layout2.addWidget(self.dropdown_1_text)
+        self.layout2.addWidget(self.dropdown_1)
+        self.layout2.addWidget(self.dropdown_2_text)
+        self.layout2.addWidget(self.dropdown_2)
+        self.layout2.addWidget(self.train_split_text)
+        self.layout2.addWidget(self.train_split_input)
+        self.layout2.addWidget(self.model_btn_text)
+        self.layout2.addWidget(self.model_btn)
+        self.layout2.addWidget(self.output_text)
+        #adding the rightside ui to the main layout
+        self.layout2.setAlignment(Qt.AlignTop)
+
+        self.layout1.addLayout(self.layout2, 20)
+
+        self.setCentralWidget(self.central_widget)
+
+    #def test_func(self):
+        #self.output_text.setText("Clicked")
 
 
 app = QApplication(sys.argv)
-ui_layout = UiLayout()
-window = MainWindow(ui_layout)
+window = MainWindow()
 window.showMaximized()
 app.exec_()
