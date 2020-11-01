29c211b7abd735a06a3607411f294b2a66b8e806
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-11-01 10:23:53 -0800

    used template inheritance

--- web_alpha_build/site_alpha.py
+++ web_alpha_build/site_alpha.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_view():
-    return render_template('index.html')
+    return render_template('noData.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
