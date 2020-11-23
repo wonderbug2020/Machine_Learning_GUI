c954ef93538a57f13a0c44c3eac35beb62e0c9aa
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-11-23 11:45:13 -0800

    added Linear regression for regression type models

--- web_alpha_2_build/site_alpha1.1.py
+++ web_alpha_2_build/site_alpha1.1.py
@@ -69,10 +69,10 @@ def index_results():
     modsel.append(session.get('Splitsel',None))
     modsel.append(session.get('transformsel',None))
     modsel.append(session.get('modelsel',None))
-    cm,ac=ml_model.run_model(getsel,modsel[0],modsel[1],modsel[2],modsel[3])
+    metric_1=ml_model.run_model(getsel,modsel[0],modsel[1],modsel[2],modsel[3])
     #print(modsel[0])
 
-    return render_template('ModelResult.html',ac=ac,cm=cm)#var=modsel[0])
+    return render_template('ModelResult.html',metric_1=metric_1)#var=modsel[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
